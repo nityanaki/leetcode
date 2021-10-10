ccd9d14aad010a9a861944d8e43ccc0861d09460
Nitya <nityanaki@gmail.com>
2021-10-10 18:18:46 -0400

    Create solution.cpp

---
+++ medium/prod_of_arr_except_self/solution.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> out;
+        //for size 2:
+        //just reverse elements
+        if (nums.size() == 2) {
+            out.push_back(nums[1]);
+            out.push_back(nums[0]);
+            return out;
+        }
+        
+        
+        // 1: (2 * 3 * 4)
+        // 2: (1) * (3*4)
+        // 3: (1* 2) * (4)
+        // 4: (1 * 2 * 3)
+        int lProd = 1;
+        vector<int> rProds;
+        int rProd = 1;
+        //iterate through nums backwards
+        for (int i = nums.size() - 1; i > 0; --i) {
+            rProd *= nums[i];
+            rProds.push_back(rProd);
+        } // content should be 
+        // 0: 4, 1: 12, 2: 24
+        // iterate through nums forward
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i != 0) {
+                lProd *= nums[i-1];
+            }
+            int r = 1;
+            if (i != nums.size() - 1)
+                r = rProds[rProds.size() - 1 - i];
+            int prod = lProd * r;
+            out.push_back(prod);
+        }
+        
+        //edge cases?
+        //[a, b]
+        //rProds: b
+        // out should be [b, a]
+        
+        return out;
+    }
+};
